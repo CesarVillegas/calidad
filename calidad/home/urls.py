cbd41be308cd2e13aef02718f39db5d6317d3b6c
cesar_campus_digital <cesar.villegas.cortes@gmail.com>
2026-03-10 11:00:32 -0300

    Se actualiza navegación y diseño del navbar

--- calidad/home/urls.py
+++ calidad/home/urls.py
@@ -5,4 +5,14 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('quienes-somos/', views.somos, name='somos'),
+
+    path('calidad/organizacion/', views.calidad_organizacion, name='calidad_organizacion'),
+    path('calidad/definicion/', views.calidad_definicion, name='calidad_definicion'),
+    path('calidad/politica/', views.calidad_politica, name='calidad_politica'),
+    path('calidad/ciclo-phva/', views.ciclo_phva, name='ciclo_phva'),
+    path('calidad/mecanismos-sgic/', views.mecanismos_sgic, name='mecanismos_sgic'),
+
+    path('formacion/', views.formacion, name='formacion'),
+    path('documentos/', views.documentos, name='documentos'),
+    path('multimedia/', views.multimedia, name='multimedia'),
 ]
--- calidad/home/views.py
+++ calidad/home/views.py
@@ -3,6 +3,29 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html')
 
-
 def somos(request):
     return render(request, 'home/quienes-somos.html')
+
+def calidad_organizacion(request):
+    return render(request, 'home/calidad_organizacion.html')
+
+def calidad_definicion(request):
+    return render(request, 'home/calidad_definicion.html')
+
+def calidad_politica(request):
+    return render(request, 'home/calidad_politica.html')
+
+def ciclo_phva(request):
+    return render(request, 'home/ciclo_phva.html')
+
+def mecanismos_sgic(request):
+    return render(request, 'home/mecanismos_sgic.html')
+
+def formacion(request):
+    return render(request, 'home/formacion.html')
+
+def documentos(request):
+    return render(request, 'home/documentos.html')
+
+def multimedia(request):
+    return render(request, 'home/multimedia.html')
