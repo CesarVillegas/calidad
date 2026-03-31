df7f86ff91433337e6a7efe4dcdb6268ba35763f
Cesar <cesar.villegas.cortes@gmail.com>
2026-03-31 12:48:54 +0000

    Refactor estructura proyecto Django (fix nesting, manage.py root)

--- calidad/settings/base.py
+++ calidad/settings/base.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'calidad.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/www/userena.cl_calidad/db.sqlite3',
     }
 }
 
